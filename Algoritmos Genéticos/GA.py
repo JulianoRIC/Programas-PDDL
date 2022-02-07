516994490cd1764cc2f3e049c0ba21b899eee6b4
Juliano_RIC <jrs96.jr@gmail.com>
2022-02-07 14:27:23 -0300

    subindo código de genetic algorithm

---
+++ Algoritmos Genéticos/GA.py
@@ -0,0 +1,55 @@
+
+#Algoritmo genético 
+
+L = 4 * 8 #size of chromossome in bits
+
+import struct
+import random
+import math
+
+
+def floatToBits(f):
+        s = struct.pack('>f', f)
+        return struct.unpack('>L', s)[0]
+
+def bitsToFloat(b):
+        s = struct.pack('>L', b)
+        return struct.unpack('>f', s)[0]
+
+#exemplo 1.23 -> '0010111100'
+
+def get_bits(x):
+        x = floatToBits(x)
+        N = 4 * 8
+        bits = ''
+        for bit in range(N):
+                b = x & (2**bit)
+                bits += '1' if b > 0 else '0'
+        return bits
+
+#exemplo '00010111100' -> 1.23
+
+def get_float(bits):
+        x = 0
+        assert(len(bits) == L)
+        for i, bit in enumerate(bits):
+                bit = int(bit)  #0 or 1
+                x += bit * (2**i)
+        return bitsToFloat(x)
+
+
+#size of population
+n = 4
+
+def population():
+        for i in range(n):
+                p = random.SystemRandom().uniform(0, math.pi)
+                if p == math.pi:
+                        p = round(p)
+                else:
+                        print(p)
+                person = get_bits(p)
+                print(person)
+
+population()
+
